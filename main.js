0bb5b61363698f5e0fb7ef980b59684757ffa7d8
Sounish Nath <40270033+sounishnath003@users.noreply.github.com>
2018-06-29 02:26:38 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+console.clear();
+
+window.onload = (function(){
+var videoEl = document.querySelector('video');
+document.querySelector('.video-button').addEventListener('click', function(){
+  if(this.dataset.aperture === 'open') {
+    this.dataset.aperture = 'closed';
+    videoEl.pause();
+    videoEl.progress = 0;
+  } else {
+    this.dataset.aperture = 'open';
+    videoEl.play();
+  }
+});
+});
+
+
